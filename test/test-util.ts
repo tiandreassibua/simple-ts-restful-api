f4387fd338a777cad39fd14b8996328b436b0f12
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2024-03-01 06:53:25 +0700

    fix: bug on test

--- test/test-util.ts
+++ test/test-util.ts
@@ -1,7 +1,7 @@
 import { prismaClient } from "../src/application/database";
 import bcrypt from "bcrypt";
 import { Address, Contact, User } from "@prisma/client";
-
+ 
 export class UserTest {
     static async delete() {
         await prismaClient.user.deleteMany({
